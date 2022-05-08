086ccbdfcb86972cc5bf62a72f81ec95f61e48de
Dwight Smith <dwightsmth@gmail.com>
2022-05-07 22:45:49 -0400

    added binary search

---
+++ src/binarySearch/binarySearch.ts
@@ -0,0 +1,37 @@
+export const binarySearchRecurse = function(arr: number[], target: number, min: number = 0, max: number = arr.length - 1){
+    let mid = Math.floor((min + max) / 2)
+    
+    if(min > max)
+        return console.log('not found')
+
+    if(arr[mid] === target)
+        return console.log(`found at index ${mid}`)
+
+    if(arr[mid] > target)
+        binarySearchRecurse(arr, target, min, mid - 1)
+
+    if(arr[mid] < target)
+        binarySearchRecurse(arr, target, mid + 1, max)
+    
+}
+
+export const binarySearchLoop = function(arr: number[], target: number){
+    let min = 0, max = arr.length -1
+
+    while(min <= max){
+        let mid = Math.floor((min + max) / 2)
+
+        if(arr[mid] === target)
+            return console.log(`found at index ${mid}`)
+
+        if(arr[mid] > target)
+            max = mid - 1
+
+        if(arr[mid] < target)
+            min = mid + 1
+
+    }
+    
+    return console.log('not found')
+    
+}
