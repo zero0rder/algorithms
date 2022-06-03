181cfa2364839ae83a66ae97ad9946ca740ab523
Dwight Smith <dwightsmth@gmail.com>
2022-06-02 21:33:49 -0400

    added additional methods

--- src/heap/heap.ts
+++ src/heap/heap.ts
@@ -1,29 +1,47 @@
 export class MaxHeap {
-    heap: number[] = [];
-    heapSize: number = 0;
+    heap: number[];
+    heapSize: number;
+    maxSize: number;
 
-    constructor(){}
+    constructor(h: number[] = [], max: number = 12){
+        this.heap = h;
+        this.heapSize = h.length;
+        this.maxSize = h.length > max ? h.length : max;
+    }
 
     insert(value: number): void {
-        // - insert where maxSize pointer is??
+        if(this.heapSize === this.maxSize)
+            return console.log('full heap :(');
+
+        this.heapSize++;
+        this.heap[this.heapSize] = value;
+        this.siftUp(this.heapSize);
+
     }
 
     siftUp(index: number): void {
-        while(index > 1 && this.heap[this.getParent(index)] < this.heap[index]){
+        while(index > 0 && this.heap[this.getParent(index)] < this.heap[index]){
             this.swap(this.heap, index, this.getParent(index));
             index = this.getParent(index);
         }
     }
 
     siftDown(index: number): void {
-        let maxIndex = index;
+        let heap = this.heap;
         let left = this.getLeftChild(index);
+        let right = this.getRightChild(index);
+        let maxIndex = index;
 
-        if(left <= this.heapSize && this.heap[index] > this.heap[maxIndex])
+        if(left <= this.heapSize && heap[left] > heap[maxIndex])
             maxIndex = left;
         
-        //todo...
-        
+        if(right <= this.heapSize && heap[right] > heap[maxIndex])
+            maxIndex = right;
+
+        if(index !== maxIndex){
+            this.swap(heap, index, maxIndex);
+            this.siftDown(maxIndex);
+        }
     }
 
     // - return max value in heap
@@ -41,20 +59,33 @@ export class MaxHeap {
 
     // - returns the max item, removing it
     extractMax(){
+        let result = this.heap[0];
+        this.swap(this.heap, result, this.heap[this.heapSize]);
+        this.heapSize--;
+        this.siftDown(0);
+        return result;
 
     }
 
     // - removes item at index
     remove(index: number){
-
+        this.heap[index] = Infinity;
+        this.siftUp(index);
+        this.extractMax();
     }
 
     // - create heap from given array items
-    heapify(a: number[]){
+    heapify(a: number[]): number[]{
+        this.heapSize = a.length;
 
+        // '(a.length / 2 - 1)' => index of last non-leaf node
+        for(let i = Math.floor((a.length / 2) - 1); i >= 0; i--)
+            this.siftDown(i);
+
+        return this.heap = a;
     }
 
-    // - take an unsorted array and turn it into a sorted array in-place using a max heap or min heap
+    // - take an unsorted array and turn it into a sorted array in-place using a max heap
     heapSort(){
 
     }
