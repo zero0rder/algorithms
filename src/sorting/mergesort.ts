0e6f8b8cce8143996708a79aa28e1e75e9c5423e
Dwight Smith <dwightsmth@gmail.com>
2022-06-13 17:17:34 -0400

    added mergesort

---
+++ src/sorting/mergesort.ts
@@ -0,0 +1,25 @@
+/**
+ *  MERGESORT  T = O(n log(n)), S = O(n)
+ */
+
+export const merge = (left: (number | undefined)[], right: (number | undefined)[]) : (number | undefined)[] => {
+    let auxArr = [];
+    
+    while(left.length && right.length){
+        if(left[0] && right[0])
+            if(left[0] < right[0]){
+                auxArr.push(left.shift())
+            } else {
+                auxArr.push(right.shift())
+            }
+    }
+
+    return [...auxArr, ...left, ...right];
+}
+
+export const mergesort = (arr: number[]): (number | undefined)[] => {
+    if(arr.length <= 1)
+        return arr;
+    
+    return merge(mergesort(arr.splice(0, ~~(arr.length / 2))), mergesort(arr));
+}
