d379e7e12bcb6a331a09a65ea1eac5e565ac12a3
Dwight Smith <dwightsmth@gmail.com>
2022-06-14 23:07:42 -0400

    selection sort

---
+++ src/sorting/selectionsort.ts
@@ -0,0 +1,31 @@
+/**
+ *  SELECTIONSORT  T = O(n^2), S = O(n)
+ */
+
+export const selectionsort = (a: number[]): number[] => {
+    let sortedArr = [];
+    let len = a.length;
+
+    for(let i=0; i < len; i++){
+        let min = findMin(a);
+        sortedArr.push(a.splice(min, 1)[0])
+    }
+
+    return sortedArr;
+
+}
+
+const findMin = (a: number[]) => {
+    let minElement = a[0];
+    let minIndex = 0;
+    let len = a.length;
+
+    for(let j=1; j < len; j++){
+        if(a[j] < minElement){
+            minElement = a[j];
+            minIndex = j;
+        }
+    }
+
+    return minIndex;
+}  
