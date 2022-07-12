f9f6bf905c13e07466d84df7a3b2c1351e8993a1
Dwight Smith <dwightsmth@gmail.com>
2022-07-11 23:49:04 -0400

    quicksort algos

---
+++ src/sorting/quicksort.ts
@@ -0,0 +1,61 @@
+/**
+ *  QUICKSORT:  T = O(n log(n)), S = O(1) -> [T = O(n^2) worst-case]
+ */
+
+export const quickSortRecursive = (a: number[], lower: number = 0, upper: number = a.length - 1): void => {
+    if(lower >= upper)
+        return;
+
+    let pivot = partition(a, lower, upper);
+    quickSortRecursive(a, lower, pivot - 1);
+    quickSortRecursive(a, pivot + 1, upper);
+}
+
+export const quicksortIterative = (a: number[]) => {
+    let stack = [];
+   
+    //add entire unsorted array as initial subarray
+    stack.push(0);
+    stack.push(a.length - 1);
+
+    while(stack[stack.length - 1] >= 0){
+        //extract topmost subarray
+        let end = stack.pop(), start = stack.pop(), pivot = partition(a, start ?? 0, end ?? (a.length - 1));
+        
+        //if there are unsorted elements to the left of the pivot,
+        //add that subarray to the stack for sorting later
+        if(start !== undefined && (pivot - 1) > start) {
+            stack.push(start);
+            stack.push(pivot - 1);
+        }
+        
+        //if there are unsorted elements to the right of the pivot,
+        //add that subarray to the stack for sorting later
+        if(end !== undefined && (pivot + 1) < end) {
+            stack.push(pivot + 1);
+            stack.push(end);
+        }
+
+    }
+
+    return a;
+
+}
+
+const partition = (a: number[], lower: number, upper: number): number => {
+    let p = upper, k = lower - 1;
+    //p = ~~(Math.random() * (upper - lower))
+
+    for(let j = lower; j <= upper; j++){
+        if(a[j] < a[p]){
+            k++;
+            [a[j], a[k]] = [a[k], a[j]];
+        }
+    }
+
+    //insert pivot into middle
+    [a[k + 1], a[p]] = [a[p], a[k + 1]];
+
+    //return pivot index
+    return k + 1;
+}
